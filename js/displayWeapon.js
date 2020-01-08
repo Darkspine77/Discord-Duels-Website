966ecbc69c5532f7c5778a9beebaf03c01cc0867
Darkspine77 <jamalcwoods@gmail.com>
2020-01-08 15:08:31 -0500

    overwriting conflicted

---
+++ js/displayWeapon.js
@@ -0,0 +1,131 @@
+
+function weaponLevelAdjust(){
+    var level = document.getElementById("weaponLevelSlider").value
+    document.getElementById("weaponLevelValue").value = level
+    adjustWeaponStatsForLevel(level)
+}
+
+function weaponLevelSet(){
+    var level = document.getElementById("weaponLevelValue").value
+    if(!isNaN(parseInt(level))){
+        level = parseInt(level)
+        if(level < 1){
+            level = 1
+            document.getElementById("weaponLevelValue").value = level
+        } else if(level > 50){
+            level = 50
+            document.getElementById("weaponLevelValue").value = level
+        }
+        document.getElementById("weaponLevelSlider").value = level;
+    }
+    adjustWeaponStatsForLevel(level)
+}
+
+function adjustWeaponStatsForLevel(level){
+    document.getElementById("weaponCost").innerHTML = "Upgrade Cost: " + data.goldToUpgrade * level
+    document.getElementById("rDamage").innerHTML = "Rush Damage: " + findScaledValue(data.moveDamage[0],level)
+    document.getElementById("sDamage").innerHTML = "Strike Damage: " + findScaledValue(data.moveDamage[1],level)
+    document.getElementById("cDamage").innerHTML = "Counter Damage: " + findScaledValue(data.moveDamage[2],level)
+    document.getElementById("weaponEffects").innerHTML = "<h1>Effects:</h1>"
+    if(data.effectIDs){
+        for(var effect of supplementaryData.effects){
+            var effectDiv = document.createElement("div")
+            
+            var effectTitle = document.createElement("h2")
+            var effectLink = document.createElement("a")
+            effectLink.href = "display.html?contentType=effect&id=" + (effect.id - 1)
+            effectLink.innerHTML = effect.name
+            effectTitle.appendChild(effectLink)
+            effectDiv.appendChild(effectTitle)
+    
+            var effectDescription = document.createElement("p")
+            effectDescription.innerHTML = createDescription(effect,level)
+            effectDiv.appendChild(effectDescription)
+    
+            document.getElementById("weaponEffects").appendChild(effectDiv)
+        }
+    }
+}
+
+function loadExternalWeaponData(callback){
+    if(data.effectIDs){
+        firebase.database().ref("effects").once('value').then(function(snapshot){
+            supplementaryData.effects = []
+            for(var id of data.effectIDs){
+                supplementaryData.effects.push(snapshot.val()[id - 1])
+            }
+            document.getElementById("weaponEffects").style.display = "block";
+            if(data.creatureIDs){
+                firebase.database().ref("creatures").once('value').then(function(snapshot){
+                    supplementaryData.creatures = []
+                    for(var id of data.creatureIDs){
+                        var creature = snapshot.val()[id - 1]
+                        var creatureData = [creature]
+                        for(var drop of creature.drops){
+                            if(drop.name == data.name){
+                                creatureData.push(drop.chance)
+                            }
+                        }
+                        supplementaryData.creatures.push(creatureData)
+                    }
+                    document.getElementById("weaponLocations").style.display = "block";
+                    callback()
+                })
+            } else {
+                callback()
+            }
+        })
+    } else if(data.creatureIDs){
+        firebase.database().ref("creatures").once('value').then(function(snapshot){
+            supplementaryData.creatures = []
+            for(var id of data.creatureIDs){
+                var creature = snapshot.val()[id - 1]
+                var creatureData = [creature]
+                for(var drop of creature.drops){
+                    if(drop.name == data.name){
+                        creatureData.push(drop.chance)
+                    }
+                }
+                supplementaryData.creatures.push(creatureData)
+            }
+            document.getElementById("weaponLocations").style.display = "block";
+            callback()
+        })
+    } else {
+        callback()
+    }   
+}
+
+function visualizeWeapon(){ 
+    console.log(data)
+    loadExternalWeaponData(function(){
+        adjustWeaponStatsForLevel(1)
+        document.getElementById("weaponLevelSlider").addEventListener("input",weaponLevelAdjust)
+        document.getElementById("weaponLevelValue").addEventListener("input",weaponLevelSet)
+        
+        document.getElementById("weaponTitle").innerHTML = data.name
+        document.getElementById("weaponTier").innerHTML = "Tier: " + data.tier
+        document.getElementById("weaponID").innerHTML = "ID: " + data.id  
+        document.getElementById("weaponDesc").innerHTML = data.description
+        if(data.creatureIDs){
+            for(var creature of supplementaryData.creatures){
+                console.log(creature)
+                var creatureDiv = document.createElement("div")
+                creatureDiv.id = "weaponLocation"
+                
+                var creatureName = document.createElement("h2")
+                var creatureLink = document.createElement("a")
+                creatureLink.href = "display.html?contentType=creature&id=" + (creature[0].id - 1)
+                creatureLink.innerHTML = creature[0].name
+                creatureName.appendChild(creatureLink)
+                creatureDiv.appendChild(creatureName)
+        
+                var creatureDescription = document.createElement("p")
+                creatureDescription.innerHTML = creature[1] + "% Drop Chance"
+                creatureDiv.appendChild(creatureDescription)
+        
+                document.getElementById("weaponLocations").appendChild(creatureDiv)
+            }
+        }
+    })
+} 
